787a4ae114ee11028e608b75d85212c1f77ba594
Augustin Chassine <augustin.chassine@mabsilico.com>
2024-12-13 18:40:34 +0100

    feat: footer (wip)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,8 @@
 	import '@typehaus/metropolis';
 	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
 	import Header from './Header.svelte';
+	import facebook_icon from '$lib/assets/facebook_icon.svg';
+	import instagram_icon from '$lib/assets/instagram_icon.svg';
 	import '../app.css';
 	let { children } = $props();
 </script>
@@ -14,6 +16,14 @@
 			{@render children()}
 		</main>
 		<footer>
+			<div>
+				<a href="https://www.facebook.com/groups/slacklille" target="_blank">
+					<img class="w-12" src="{facebook_icon}" alt="logo facebook" />
+				</a>
+				<a href="https://www.instagram.com/slack_lille" target="_blank">
+					<img class="w-12" src="{instagram_icon}" alt="logo instagram" />
+				</a>
+			</div>
 			&copy; {new Date().getFullYear()} - SlackLille
 		</footer>
 	</div>
