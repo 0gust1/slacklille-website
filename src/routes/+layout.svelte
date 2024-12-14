774673900fab84ccb41a115e68adc2020231b6c5
Augustin Chassine <augustin.chassine@mabsilico.com>
2024-12-14 19:28:11 +0100

    fix: extract footer in its own component, fix links

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,8 +3,7 @@
 	import '@typehaus/metropolis';
 	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
 	import Header from './Header.svelte';
-	import facebook_icon from '$lib/assets/facebook_icon.svg';
-	import instagram_icon from '$lib/assets/instagram_icon.svg';
+	import Footer from './Footer.svelte';
 	import '../app.css';
 	let { children } = $props();
 </script>
@@ -15,26 +14,7 @@
 		<main>
 			{@render children()}
 		</main>
-		<footer>
-			<div class="nav-social">
-				<nav>
-					<ul>
-						<li><a href="festival">▶ Festival F<sup>3</sup></a></li>
-						<li><a href="ressources">▶ Ressources</a></li>
-						<li><a href="mailto:slacklille+contact@gmail.com">✉️ Contact</a></li>
-					</ul>
-				</nav>
-				<div class="social-links">
-					<a href="https://www.facebook.com/groups/slacklille" target="_blank">
-						<img class="w-12" src={facebook_icon} alt="logo facebook" />
-					</a>
-					<a href="https://www.instagram.com/slack_lille" target="_blank">
-						<img class="w-12" src={instagram_icon} alt="logo instagram" />
-					</a>
-				</div>
-			</div>
-			&copy; {new Date().getFullYear()} - SlackLille
-		</footer>
+		<Footer />
 	</div>
 </ParaglideJS>
 
@@ -44,18 +24,5 @@
 	}
 	main {
 		/* @apply py-10 bg-slackgreen-200; */
-	}
-
-	footer {
-		@apply py-10 bg-gray-200 px-4;
-		.nav-social{
-			@apply flex items-center justify-between  text-xs;
-		}
-		nav {
-			@apply mb-5 text-2xl;
-		}
-		.social-links {
-			@apply flex gap-2;
-		}
 	}
 </style>
---
+++ src/routes/Footer.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+  import { base } from '$app/paths';
+	import facebook_icon from '$lib/assets/facebook_icon.svg';
+	import instagram_icon from '$lib/assets/instagram_icon.svg';
+</script>
+
+<footer>
+	<div class="nav-social">
+		<nav>
+			<ul>
+				<li><a href="{base}/festival">▶ Festival F<sup>3</sup></a></li>
+				<li><a href="{base}/ressources">▶ Ressources</a></li>
+				<li><a href="mailto:slacklille+contact@gmail.com">✉️ Contact</a></li>
+			</ul>
+		</nav>
+		<div class="social-links">
+			<a href="https://www.facebook.com/groups/slacklille" target="_blank">
+				<img class="w-12" src={facebook_icon} alt="logo facebook" />
+			</a>
+			<a href="https://www.instagram.com/slack_lille" target="_blank">
+				<img class="w-12" src={instagram_icon} alt="logo instagram" />
+			</a>
+		</div>
+	</div>
+	&copy; {new Date().getFullYear()} - SlackLille
+</footer>
+
+<style lang="postcss">
+	footer {
+		@apply bg-gray-200 px-4 py-10;
+		.nav-social {
+			@apply flex items-center justify-between text-xs;
+		}
+		nav {
+			@apply mb-5 text-2xl;
+		}
+		.social-links {
+			@apply flex gap-2;
+		}
+	}
+</style>
