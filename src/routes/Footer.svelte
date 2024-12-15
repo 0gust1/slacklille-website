7ffadf377bfe86ffa3a6179e58c4429af1c13d82
Augustin Chassine <augustin.chassine@mabsilico.com>
2024-12-15 03:11:17 +0100

    feat: footer overhaul + header adj

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -3,45 +3,65 @@
 	import FacebookIcon from '$lib/components/FacebookIcon.svelte';
 	import InstagramIcon from '$lib/components/InstagramIcon.svelte';
 	import EmailIcon from '$lib/components/EmailIcon.svelte';
+	import slacklille_logo from '$lib/assets/Logo_SlackLille.svg';
 </script>
 
 <footer>
-	<div class="nav-social">
-		<nav>
-			<ul>
-				<li><a href="{base}/festival">▶ Festival F<sup>3</sup></a></li>
-				<li><a href="{base}/ressources">▶ Ressources</a></li>
-				<li>
-					<a href="mailto:slacklille+contact@gmail.com">
-						<EmailIcon class="inline-block size-8" />
-						<span class="">Contact</span>
-					</a>
-				</li>
-			</ul>
-		</nav>
-		<div class="social-links">
-			<a href="https://www.facebook.com/groups/slacklille" target="_blank">
-				<FacebookIcon />
-			</a>
-			<a href="https://www.instagram.com/slack_lille" target="_blank">
-				<InstagramIcon />
-			</a>
+	<div class="mx-auto max-w-7xl px-6 pb-8 lg:px-8">
+		<div
+			class="grid auto-cols-max grid-flow-col place-content-start gap-5 border-t border-gray-900/10 pt-12 xl:gap-8"
+		>
+			<div class="">
+				<img class=" w-40 pr-4" src={slacklille_logo} alt="SlackLille" />
+			</div>
+			<div class="grow-0">
+				<a class="text-sm/6 font-semibold text-gray-950" href="{base}/"> SlackLille </a>
+				<ul role="list" class="mt-4 space-y-3">
+					<li>
+						<a href="{base}/festival" class="text-sm/6 text-gray-600 hover:text-gray-900"
+							>Festival F<sup>3</sup></a
+						>
+					</li>
+					<li>
+						<a href="{base}/ressources" class="text-sm/6 text-gray-600 hover:text-gray-900"
+							>Ressources</a
+						>
+					</li>
+					<li>
+						<a href="{base}/identity" class="text-sm/6 text-gray-600 hover:text-gray-900"
+							>Identité Graphique</a
+						>
+					</li>
+				</ul>
+			</div>
+			<div class="grow-0">
+				<a href="mailto:slacklille+contact@gmail.com">
+					<span class="text-sm font-semibold">Contact</span>
+					<EmailIcon class="mt-4 size-8 text-gray-600" />
+				</a>
+			</div>
+		</div>
+		<div class="mt-8 border-t border-gray-900/10 pt-8 md:flex md:items-center md:justify-between">
+			<div class="flex gap-x-6 md:order-2">
+				<a
+					href="https://www.facebook.com/groups/slacklille"
+					class="text-gray-600 hover:text-gray-800"
+				>
+					<FacebookIcon class="size-10" />
+				</a>
+				<a href="https://www.instagram.com/slack_lille/" class="text-gray-600 hover:text-gray-800">
+					<InstagramIcon class="size-10" />
+				</a>
+			</div>
+			<p class="mt-8 text-sm/6 text-gray-600 md:order-1 md:mt-0">
+				&copy; {new Date().getFullYear()} SlackLille
+			</p>
 		</div>
 	</div>
-	&copy; {new Date().getFullYear()} - SlackLille
 </footer>
 
 <style lang="postcss">
 	footer {
-		@apply bg-gray-200 px-4 py-10;
-		.nav-social {
-			@apply flex items-center justify-between text-xs;
-		}
-		nav {
-			@apply mb-5 text-2xl;
-		}
-		.social-links {
-			@apply flex gap-2;
-		}
+		@apply bg-slackblue-100;
 	}
 </style>
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -5,46 +5,74 @@
 	import InstagramIcon from '$lib/components/InstagramIcon.svelte';
 	import EmailIcon from '$lib/components/EmailIcon.svelte';
 	import slacklille_logo from '$lib/assets/Logo_SlackLille.svg';
-
+	import { slide } from 'svelte/transition';
 	let menuOpen = $state(false);
 </script>
 
 <header>
-  <nav class="mx-auto flex max-w-7xl items-center justify-between px-6 pt-4 md:px-8" aria-label="Global">
-    <div class="flex items-end gap-x-12">
-      <a href="{base}/" class="-m-1.5 p-1.5">
-        <span class="sr-only">SlackLille</span>
-        <img src={slacklille_logo} alt="logo SlackLille" class="w-40 -mb-1" />
-      </a>
+	<nav
+		class="mx-auto flex max-w-7xl items-center justify-between px-6 pt-4 md:px-8"
+		aria-label="Global"
+	>
+		<div class="flex items-end gap-x-12">
+			<a href="{base}/" class="-m-1.5 p-1.5">
+				<span class="sr-only">SlackLille</span>
+				<img src={slacklille_logo} alt="logo SlackLille" class="-mb-1 w-40" />
+			</a>
 			<ul class="internal-nav hidden md:flex md:gap-x-12">
 				<li>
-					<a href="{base}/" class:active={$page.url.pathname === `${base}/`} class="text-sm/6 font-semibold text-gray-900">Accueil</a></li>
-				<li>
-					<a href="{base}/festival" class:active={$page.url.pathname === `${base}/festival`} class="text-sm/6 font-semibold text-gray-900"
-						>Festival&nbsp;F<sup>3</sup></a
+					<a
+						href="{base}/"
+						class:active={$page.url.pathname === `${base}/`}
+						class="text-sm/6 font-semibold text-gray-900">Accueil</a
 					>
 				</li>
 				<li>
-					<a href="{base}/ressources" class:active={$page.url.pathname === `${base}/ressources`} class="text-sm/6 font-semibold text-gray-900"
-						>Ressources</a
+					<a
+						href="{base}/festival"
+						class:active={$page.url.pathname === `${base}/festival`}
+						class="text-sm/6 font-semibold text-gray-900">Festival&nbsp;F<sup>3</sup></a
+					>
+				</li>
+				<li>
+					<a
+						href="{base}/ressources"
+						class:active={$page.url.pathname === `${base}/ressources`}
+						class="text-sm/6 font-semibold text-gray-900">Ressources</a
 					>
 				</li>
 			</ul>
-    </div>
-    <div class="flex md:hidden">
-      <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700" onclick={() => menuOpen = !menuOpen}>
-        <span class="sr-only">Open main menu</span>
-        <svg class="size-8" fill="none" viewBox="0 0 24 24" stroke-width="1.8" stroke="currentColor" aria-hidden="true" data-slot="icon">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-        </svg>
-      </button>
-    </div>
-    <div class="hidden md:flex self-end items-end mb-2">
-      <ul class="social-links">
+		</div>
+		<div class="flex md:hidden">
+			<button
+				type="button"
+				class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+				onclick={() => (menuOpen = !menuOpen)}
+			>
+				<span class="sr-only">Open main menu</span>
+				<svg
+					class="size-8"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke-width="1.8"
+					stroke="currentColor"
+					aria-hidden="true"
+					data-slot="icon"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+					/>
+				</svg>
+			</button>
+		</div>
+		<div class="mb-2 hidden items-end self-end md:flex">
+			<ul class="social-links">
 				<li>
 					<a href="mailto:slacklille+contact@gmail.com">
 						<EmailIcon class="size-8" />
-						<span class='sr-only'>Contact</span>
+						<span class="sr-only">Contact</span>
 					</a>
 				</li>
 				<li>
@@ -58,78 +86,107 @@
 					</a>
 				</li>
 			</ul>
-    </div>
-  </nav>
-  <!-- Mobile menu, show/hide based on menu open state. -->
+		</div>
+	</nav>
+	<!-- Mobile menu, show/hide based on menu open state. -->
 	{#if menuOpen}
-  <div class="md:hidden" role="dialog" aria-modal="true">
-    <!-- Background backdrop, show/hide based on slide-over state. -->
-    <div class="fixed inset-0 z-10"></div>
-    <div class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-      <div class="flex items-center justify-between">
-				<a href="{base}/" class="-m-1.5 p-1.5">
-					<span class="sr-only">SlackLille</span>
-					<img src={slacklille_logo} alt="logo SlackLille" class=" w-32 -mb-1" />
-				</a>
-        <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700" onclick={()=>menuOpen = false}>
-          <span class="sr-only">Close menu</span>
-          <svg class="size-8" fill="none" viewBox="0 0 24 24" stroke-width="1.8" stroke="currentColor" aria-hidden="true" data-slot="icon">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
-          </svg>
-        </button>
-      </div>
-      <div class="mt-6 flow-root">
-        <div class="-my-6 divide-y divide-gray-500/10">
-					<ul class="space-y-2 py-6">
-						<li>
-							<a href="{base}/" class:active={$page.url.pathname === `${base}/`} class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50" onclick={()=>{menuOpen=!menuOpen}}>Accueil</a></li>
-						<li>
-							<a href="{base}/festival" class:active={$page.url.pathname === `${base}/festival`} class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50" onclick={()=>{menuOpen=!menuOpen}}
-								>Festival&nbsp;F<sup>3</sup></a
-							>
-						</li>
-						<li>
-							<a href="{base}/ressources" class:active={$page.url.pathname === `${base}/ressources`} class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50" onclick={()=>{menuOpen=!menuOpen}}
-								>Ressources</a
-							>
-						</li>
-					</ul>
-          <div class="py-6">
-						<ul class="social-links">
+		<div class="md:hidden" role="dialog" aria-modal="true" transition:slide>
+			<!-- Background backdrop, show/hide based on slide-over state. -->
+			<div class="fixed inset-0 z-10 bg-slackblue-950/90"></div>
+			<div
+				class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10"
+			>
+				<div class="flex items-center justify-between">
+					<a href="{base}/" class="-m-1.5 p-1.5">
+						<span class="sr-only">SlackLille</span>
+						<img src={slacklille_logo} alt="logo SlackLille" class=" -mb-1 w-32" />
+					</a>
+					<button
+						type="button"
+						class="-m-2.5 rounded-md p-2.5 text-gray-700"
+						onclick={() => (menuOpen = false)}
+					>
+						<span class="sr-only">Close menu</span>
+						<svg
+							class="size-8"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke-width="1.8"
+							stroke="currentColor"
+							aria-hidden="true"
+							data-slot="icon"
+						>
+							<path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+						</svg>
+					</button>
+				</div>
+				<div class="mt-6 flow-root">
+					<div class="-my-6 divide-y divide-gray-500/10">
+						<ul class="space-y-2 py-6">
 							<li>
-								<a href="mailto:slacklille+contact@gmail.com">
-									<EmailIcon class="size-10" />
-									<span class='sr-only'>Contact</span>
-								</a>
+								<a
+									href="{base}/"
+									class:active={$page.url.pathname === `${base}/`}
+									class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+									onclick={() => {
+										menuOpen = !menuOpen;
+									}}>Accueil</a
+								>
 							</li>
 							<li>
-								<a href="https://www.facebook.com/groups/slacklille" target="_blank">
-									<FacebookIcon class=" size-10" />
-								</a>
+								<a
+									href="{base}/festival"
+									class:active={$page.url.pathname === `${base}/festival`}
+									class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+									onclick={() => {
+										menuOpen = !menuOpen;
+									}}>Festival&nbsp;F<sup>3</sup></a
+								>
 							</li>
 							<li>
-								<a href="https://www.instagram.com/slack_lille/" target="_blank">
-									<InstagramIcon class=" size-10" />
-								</a>
+								<a
+									href="{base}/ressources"
+									class:active={$page.url.pathname === `${base}/ressources`}
+									class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+									onclick={() => {
+										menuOpen = !menuOpen;
+									}}>Ressources</a
+								>
 							</li>
-							
 						</ul>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+						<div class="py-6">
+							<ul class="social-links">
+								<li>
+									<a href="mailto:slacklille+contact@gmail.com">
+										<EmailIcon class="size-10" />
+										<span class="sr-only">Contact</span>
+									</a>
+								</li>
+								<li>
+									<a href="https://www.facebook.com/groups/slacklille" target="_blank">
+										<FacebookIcon class=" size-10" />
+									</a>
+								</li>
+								<li>
+									<a href="https://www.instagram.com/slack_lille/" target="_blank">
+										<InstagramIcon class=" size-10" />
+									</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 	{/if}
 </header>
-
 
 <style lang="postcss">
 	header {
 		@apply bg-gradient-to-r from-gray-100 to-gray-500 shadow-sm;
 
 		.social-links {
-			@apply flex gap-4 items-end;
+			@apply flex items-end gap-4;
 		}
 	}
-
 </style>
